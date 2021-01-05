3d6d4050cd81a447ef17a4bc7737b9db091fc50f
Muhammad Saqib Hussain <saqib9217@live.com>
2021-01-05 07:51:02 +0500

    Rust Practice

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main(){
+    loop{
+        println!("Guess the number!");
+        let mut guess = String::new();
+        let number = rand::thread_rng().gen_range(1, 1000);
+        println!("The secret number is {}", number);
+        io::stdin().read_line(&mut guess).expect("Failed to read line!");
+        let guess:u16 = guess.trim().parse().expect("Failed!");
+        if guess == number {
+            println!("Correct!");
+        } else {
+            println!("Wrong!");
+        }
+    }
+}
