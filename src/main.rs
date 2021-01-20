32d833888cc1602788e5f875402de787e31fd771
Muhammad Saqib Hussain <saqib9217@live.com>
2021-01-20 13:05:49 +0500

    doing PE Problem 7

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main(){
-    
+    let mut num_of_prime = 0;
+    let mut num = 3;
+    loop {
+        for i in 2..num+1{
+            if num%1 == 0 && num%num==0 || num%i!=0 {
+                num_of_prime +=1;
+            } else if num_of_prime == 6 {
+                println!("{}", num);
+                break;
+            }
+            num+=1;
+        }
+        break;
+    }
 }
